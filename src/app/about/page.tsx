f807d37107ec342f3b66d1b624753b2217f00f96
Darryl Morley <darrylmorley.uk@gmail.com>
2024-06-20 09:25:38 +0100

    add about page template

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,7 +1,12 @@
 export default function Page() {
   return (
-    <div>
-      <h1>About</h1>
-    </div>
+    <>
+      <div className="px-4 text-center flex flex-col justify-center items-center h-56 bg-secondary text-white space-y-4">
+        <h1 className="text-4xl lg:text-5xl uppercase font-black">About</h1>
+        <p className="text-lg">
+          Holosun has been delivering high quality red dot optics since 2013
+        </p>
+      </div>
+    </>
   );
 }
