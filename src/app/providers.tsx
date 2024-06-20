e38d7c376c5216728a488bdb49f8703b3cd8360f
Darryl Morley <darrylmorley.uk@gmail.com>
2024-06-20 16:07:00 +0100

    remove posthog

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,20 +1,6 @@
 "use client";
-import posthog from "posthog-js";
-import { PostHogProvider } from "posthog-js/react";
 import { CartProvider } from "react-use-cart";
 
-if (typeof window !== "undefined") {
-  posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY, {
-    api_host: "/ingest",
-    ui_host: "https://eu.posthog.com",
-    person_profiles: "identified_only",
-  });
-}
-
 export function Providers({ children }) {
-  return (
-    <PostHogProvider client={posthog}>
-      <CartProvider id="holosun-cart">{children}</CartProvider>
-    </PostHogProvider>
-  );
+  return <CartProvider id="holosun-cart">{children}</CartProvider>;
 }
