ca3493723b0233a45ad4557f29eccfd7cc3054da
Darryl Morley <darrylmorley.uk@gmail.com>
2024-07-08 18:01:27 +0100

    fix list

--- src/components/menu-drawer.tsx
+++ src/components/menu-drawer.tsx
@@ -35,7 +35,6 @@ export default function MenuDrawer() {
               Shop
             </Link>
           </li>
-          {/* <li className="py-3 border-b"><Link href="/blog">Blog</Link></li> */}
           <li className="py-3 border-b">
             <Link
               href="/about"
@@ -45,7 +44,7 @@ export default function MenuDrawer() {
               About
             </Link>
           </li>
-          <div className="pl-4 mt-12">
+          <li className=" mt-12">
             <p className="font-bold underline">Need help?</p>
             <p className="mt-3">
               Email:{" "}
@@ -65,8 +64,8 @@ export default function MenuDrawer() {
                 01527831261
               </a>
             </p>
-          </div>
-          <div className="flex ml-4 mt-4 gap-3">
+          </li>
+          <li className="flex flex-row mt-4">
             <Link
               href="https://www.facebook.com/holosunuk"
               target="_blank"
@@ -112,7 +111,7 @@ export default function MenuDrawer() {
                 <path d="M0 0v24h24v-24h-24zm18.862 9.237c.208 4.617-3.235 9.765-9.33 9.765-1.854 0-3.579-.543-5.032-1.475 1.742.205 3.48-.278 4.86-1.359-1.437-.027-2.649-.976-3.066-2.28.515.098 1.021.069 1.482-.056-1.579-.317-2.668-1.739-2.633-3.26.442.246.949.394 1.486.411-1.461-.977-1.875-2.907-1.016-4.383 1.619 1.986 4.038 3.293 6.766 3.43-.479-2.053 1.079-4.03 3.198-4.03.944 0 1.797.398 2.396 1.037.748-.147 1.451-.42 2.085-.796-.245.767-.766 1.41-1.443 1.816.664-.08 1.297-.256 1.885-.517-.44.656-.997 1.234-1.638 1.697z" />
               </svg>
             </Link>
-          </div>
+          </li>
         </ul>
       </div>
     </div>
