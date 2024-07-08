a459164bae30070cac73bb55f981da331707dbd1
Darryl Morley <darrylmorley.uk@gmail.com>
2024-07-08 11:55:07 +0100

    use next Image in parralax component

--- src/components/parallax.tsx
+++ src/components/parallax.tsx
@@ -1,13 +1,21 @@
+import Image from "next/image";
+
 export default function Parallax({ imageSrc, text }) {
   return (
     <div className="relative overflow-hidden h-[70dvh]">
-      <div
+      {/* <div
         className="absolute -top-0 left-0 w-full h-full bg-center bg-fixed bg-no-repeat"
         style={{
           backgroundImage: `url(${imageSrc})`,
           backgroundAttachment: "fixed",
           backgroundSize: "cover",
         }}
+      /> */}
+      <Image
+        src={imageSrc}
+        alt="Man aiming rifle furnished with a Holosun Red Dot"
+        fill
+        className="object-cover"
       />
       <div className="relative z-10 flex items-center justify-center h-full w-full bg-black bg-opacity-50">
         <h2 className="px-8 lg:px-28 text-3xl lg:text-4xl font-black text-white">{text}</h2>
