b074c8bfe4db84488c907ea7f432fedbfbfbbdeb
Darryl Morley <darrylmorley.uk@gmail.com>
2024-07-09 13:47:21 +0100

    update hero to make Image component generate smaller image for mobile

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -10,8 +10,8 @@ export default function Hero() {
     props: { srcSet: desktop },
   } = getImageProps({
     ...common,
-    width: 1920,
-    height: 860,
+    width: 0,
+    height: 0,
     quality: 80,
     src: "/images/hero-carousel/aems-banner-1920x860.webp",
   });
@@ -19,8 +19,8 @@ export default function Hero() {
     props: { srcSet: mobile, ...rest },
   } = getImageProps({
     ...common,
-    width: 1080,
-    height: 1350,
+    width: 0,
+    height: 0,
     quality: 70,
     src: "/images/hero-carousel/aems-banner-1080x1350.webp",
   });
@@ -32,14 +32,15 @@ export default function Hero() {
         srcSet={desktop}
       />
       <source
-        media="(min-width: 500px)"
+        media="(min-width: 320px)"
         srcSet={mobile}
       />
       <Image
         {...rest}
         alt={common.alt}
-        style={{ width: "100%", height: "auto", objectFit: "cover" }}
         priority
+        sizes="100vw"
+        className="w-full h-auto object-cover"
       />
     </picture>
   );
