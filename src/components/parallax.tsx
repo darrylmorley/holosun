4ef9d367ca20814adbf130cec7f4860715f00eff
Darryl Morley <darrylmorley.uk@gmail.com>
2024-07-09 16:02:15 +0100

    apply sizes for improved mobile perf

--- src/components/parallax.tsx
+++ src/components/parallax.tsx
@@ -3,19 +3,12 @@ import Image from "next/image";
 export default function Parallax({ imageSrc, text }) {
   return (
     <div className="relative overflow-hidden h-[70dvh]">
-      {/* <div
-        className="absolute -top-0 left-0 w-full h-full bg-center bg-fixed bg-no-repeat"
-        style={{
-          backgroundImage: `url(${imageSrc})`,
-          backgroundAttachment: "fixed",
-          backgroundSize: "cover",
-        }}
-      /> */}
       <Image
         src={imageSrc}
         alt="Man aiming rifle furnished with a Holosun Red Dot"
         fill
         className="object-cover"
+        sizes="100vw"
       />
       <div className="relative z-10 flex items-center justify-center h-full w-full bg-black bg-opacity-50">
         <h2 className="px-8 lg:px-28 text-3xl lg:text-4xl font-black text-white">{text}</h2>
