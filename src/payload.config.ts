21741219f645bd0f1b8c7642db1e3fc679128097
Darryl Morley <darrylmorley.uk@gmail.com>
2025-06-09 15:56:09 +0100

    fix pnpm links regenerate payload types add s3Storage plugin for payload

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -1,6 +1,6 @@
 // storage-adapter-import-placeholder
+import { s3Storage } from "@payloadcms/storage-s3";
 import { sqliteAdapter } from "@payloadcms/db-sqlite";
-import { payloadCloudPlugin } from "@payloadcms/payload-cloud";
 import { lexicalEditor } from "@payloadcms/richtext-lexical";
 import path from "path";
 import { buildConfig } from "payload";
@@ -35,7 +35,21 @@ export default buildConfig({
   }),
   sharp,
   plugins: [
-    payloadCloudPlugin(),
-    // storage-adapter-placeholder
+    s3Storage({
+      collections: {
+        media: {
+          prefix: "holosun-blog",
+        },
+      },
+      bucket: process.env.S3_SPACE_NAME,
+      config: {
+        credentials: {
+          accessKeyId: process.env.S3_ACCESS_KEY,
+          secretAccessKey: process.env.S3_SECRET_KEY,
+        },
+        region: process.env.S3_REGION,
+        endpoint: process.env.S3_ENDPOINT,
+      },
+    }),
   ],
 });
